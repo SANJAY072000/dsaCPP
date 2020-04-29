a7f7175cdeca3c4ef968b20766b3ceba01478244
SANJAY SINGH BISHT <39243846+SANJAY072000@users.noreply.github.com>
2020-04-29 18:35:37 +0530

    My competitive coding template

---
+++ cpTemplate.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define tt typedef
+tt long long ll;
+tt long double ld;
+tt string str;
+tt vector<ll> vl;
+tt vector<vector<ll>> vvl;
+tt map<char,ll> mpc;
+tt map<ll,ll> mpl;
+tt unordered_map<char,ll> umpc;
+tt unordered_map<ll,ll> umpl;
+tt set<ll> stl;
+tt set<char> stc;
+tt unordered_set<ll> ustl;
+tt unordered_set<char> ustc;
+tt deque<ll> dql;
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define pu push
+#define po pop
+#define ft first
+#define sd second
+#define ins insert
+#define ers erase
+#define nlp cout<<"\n"
+#define bclz __builtin_clz
+#define bctz __builtin_ctz
+#define bpc __builtin_popcount
+#define bpry __builtin_parity
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define mod 1000000007
+#define solve(t) cin>>t;while(t--)
+int main() {
+	fast;
+	ll t;solve(t){
+	 
+	nlp;   
+	}
+	return 0;
+}
